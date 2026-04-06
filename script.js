1ccdb47b7568966ce380daa3897819faa579f452
Matheus Merechia Martins <matheusmerechia@gmail.com>
2026-04-06 17:58:18 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+//imagens
+
+const imagens = [
+    {src: "img/lontra.jpg", tipo: "real"},
+    {src: "img/IALontra.png", tipo: "fake"},
+]
+
+
+
+//estado do jogo
+const img=document.getElementById("#imagem img");
+const resultado=document.getElementById("resultado");
+const contador=document.getElementById("contador");
+
+let imagemAtual = 0;
+let score = 0;
+
+//funções
+function mostrarImagem() {
+    const imagem = imagens[imagemAtual];
+    img.src = imagem.src;
+    resultado.textContent = "";
+}
+
+//inicialização 
+
+mostrarImagem();
+
 
